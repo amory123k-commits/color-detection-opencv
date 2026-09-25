5579fd0fa87c3557fb945c1df7d103730a7b1316
amory123k-commits <amory123k@gmail.com>
2026-09-25 16:49:09 +0300

    Update Computer_vision.py

--- Computer_vision.py
+++ Computer_vision.py
@@ -1,18 +1,21 @@
-﻿import numpy as np
+import numpy as np
 import cv2
 from PIL import Image
-#usung get_limits from util.py
+from collections import deque
+# using get_limits from util.py
 from util import get_limits
 
-#define the color to be detected in BGR format
+# define the color to be detected in BGR format
 color=np.array([51,153,255])
 
 
-#initialize the defualt webcam
+# initialize the defualt webcam
 webcam=cv2.VideoCapture(0)
 # get the lower and upper limit from method get_limits
 lower_limit,upper_limit=get_limits(color)
+# create queue to calc last 5 avg positions to improve time complexity 
 
+positions=deque(maxlen=5)
 while True:
     ret,frame=webcam.read()
     
@@ -27,9 +30,21 @@ while True:
         box=mask_.getbbox()
         
         if box is not None:
-            x1,y1,x2,y2=box
+            positions.append(box)
+            if len(positions)==5:
+                # calculate average of last 5 positions (Smooth rectangle)
+                avg_x1=sum(p[0] for p in positions)//5
+                avg_y1=sum(p[1] for p in positions)//5
+                avg_x2=sum(p[2] for p in positions)//5
+                avg_y2=sum(p[3] for p in positions)//5
             #drawing a rectangle around the detected color 
-            frame = cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
+                frame = cv2.rectangle(frame,(avg_x1,avg_y1),(avg_x2,avg_y2),(0,255,0),2)
+            else:
+                x1,y1,x2,y2=box
+                frame=cv2.rectangle(frame,(x1,y1),(x2,y2),(0, 255, 0), 2)
+        else:
+            # when is color disappear
+            positions.clear()
         
         
         cv2.imshow("Webcam",frame)
@@ -41,8 +56,8 @@ while True:
         
         
 #release the webcam and close all windows
+webcam.release()
 cv2.destroyAllWindows()      
 
     
-
 
